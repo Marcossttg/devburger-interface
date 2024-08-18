3a1447c192d7ca87e5bcc43f4a933eefb331118b
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-08-18 00:01:23 -0300

    initiated form with react hook form

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,3 +1,10 @@
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+import Logo from '../../assets/logo.svg'
+import { Button } from "../../components/Button"
+
 import {
   Container,
   Form,
@@ -7,10 +14,23 @@ import {
   Title
 } from "./styles"
 
-import Logo from '../../assets/logo.svg'
-import { Button } from "../../components/Button"
-
 export function Login() {
+  const schema = yup
+    .object({
+      email: yup.string().email().required(),
+      password: yup.string().min(6).required(),
+    })
+    .required()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+  const onSubmit = (data) => console.log(data)
+
   return (
     <Container>
       <LeftContainer>
@@ -19,19 +39,21 @@ export function Login() {
       <RightContainer>
         <Title>Olá, seja bem vindo ao <span>Dev Burguer!</span>
           <br></br>
-          Acesse com seu <span>Login e senha.</span></Title>
-        <Form>
+          Acesse com seu <span>Login e senha.</span>
+        </Title>
+
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <InputContainer>
             <label>Email</label>
-            <input type="email" />
+            <input type="email" {...register('email')} />
           </InputContainer>
 
           <InputContainer style={{ marginTop: 10 }}>
             <label>Senha</label>
-            <input type="password" />
+            <input type="password" {...register('password')} />
           </InputContainer>
 
-          <Button >Entrar</Button>
+          <Button type="submit">Entrar</Button>
         </Form>
 
         <p>Não possui conta? <a>Clique aqui.</a></p>
